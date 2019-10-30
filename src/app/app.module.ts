886274c5a27ba6dfc2f255a051857b362bd452e7
SanketD92 <sdharwa1@binghamton.edu>
2019-10-29 22:37:24 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,31 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RestComponent } from './rest/rest.component';
+import { WorkoutComponent } from './workout/workout.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      RestComponent,
+      WorkoutComponent,
+      SettingsComponent,
+      ProgressComponent
+   ],
+   entryComponents: [],
+   imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule
+   ],
+   providers: [
+      StatusBar,
+      SplashScreen
+   ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
