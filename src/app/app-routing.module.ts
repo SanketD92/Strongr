02465c4b64de45556f334af266c9faebfdf942de
Sanket Dharwadkar <sanket.dharwadkar@alere.com>
2019-10-30 16:03:24 -0400

    app routing fix needed - rest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', loadChildren: () => import('../pages/login/login.module').then( m => m.LoginModule)},
+  { path: '', redirectTo: 'rest', pathMatch: 'full' },
+  { path: 'rest', loadChildren: () => import('../pages/rest/rest.module').then( m => m.RestModule)},
 ];
 
 @NgModule({
---
+++ src/pages/rest/rest.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestComponent } from './rest.component';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path:'',
+        component: RestComponent
+      }
+    ]),
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick: false
+    })
+  ],
+  declarations: [RestComponent]
+})
+export class RestModule { }
