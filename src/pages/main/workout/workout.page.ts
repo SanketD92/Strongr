7a22cb3d1435eaf4d14c993691cdd678b560b796
SanketD92 <sdharwa1@binghamton.edu>
2019-10-30 19:22:15 -0400

    main page created

---
+++ src/pages/main/workout/workout.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  templateUrl: 'workout.page.html',
+  styleUrls: ['workout.page.scss']
+})
+export class WorkoutPage {
+
+  constructor() {}
+
+}
