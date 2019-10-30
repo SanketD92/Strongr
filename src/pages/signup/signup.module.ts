f30f394eed5a9bdb880a8c74210441ea5081f58c
Sanket Dharwadkar <sanket.dharwadkar@alere.com>
2019-10-30 15:06:06 -0400

    signup module

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup.component';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: SignupComponent
+      }
+    ])
   ],
   declarations: [SignupComponent]
 })
