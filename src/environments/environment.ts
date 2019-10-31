04986b404f98a3fe59eb010ac3e4f7c3504a156d
Sanket Dharwadkar <sanket.dharwadkar@alere.com>
2019-10-31 11:42:02 -0400

    adding firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAJjcbkw_-C-lTAz1nfF7W0hWHjum-zjZM",
+    authDomain: "strongr-9a8e3.firebaseapp.com",
+    databaseURL: "https://strongr-9a8e3.firebaseio.com",
+    projectId: "strongr-9a8e3",
+    storageBucket: "strongr-9a8e3.appspot.com",
+    messagingSenderId: "725992152763",
+    appId: "1:725992152763:web:4e6db681d06c0e9b01939e",
+    measurementId: "G-5YGRJNPYN1"
+  }
 };
 
 /*
