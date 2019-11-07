79d696bf395433f80eaa71004efd2606e2f2cd97
SanketD92 <sdharwa1@binghamton.edu>
2019-11-06 21:58:55 -0500

    Attempting to store to firebase DB

--- src/app/main/settings/settings.page.ts
+++ src/app/main/settings/settings.page.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { AngularFireModule, FirebaseApp } from '@angular/fire';
 
 @Component({
   selector: 'app-settings',
   templateUrl: 'settings.page.html',
   styleUrls: ['settings.page.scss']
 })
-export class SettingsPage {
+export class SettingsPage implements OnInit{
+  Tgl;
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) {}
+  constructor(public afAuth: AngularFireAuth, private router: Router, private fa: FirebaseApp) {}
+
+  ngOnInit() {
+  }
+  
+  saveChanges(){
+    
+  }
 
   signOut() {
     this.afAuth.auth.signOut().then(() => {
